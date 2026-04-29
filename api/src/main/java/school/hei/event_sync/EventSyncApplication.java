865f5ce648d1c383f423880052d43fccbd867956
nassaigael <hei.nassai@gmail.com>
2026-04-29 15:52:37 +0300

    feat(api): initialise a backend Spring Boot

---
+++ api/src/main/java/school/hei/event_sync/EventSyncApplication.java
@@ -0,0 +1,13 @@
+package school.hei.event_sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventSyncApplication.class, args);
+    }
+
+}
