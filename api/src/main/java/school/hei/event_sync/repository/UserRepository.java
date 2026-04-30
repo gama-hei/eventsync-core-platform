73865006ac15756f438bcbc228aa5ef50b98ac78
nassaigael <hei.nassai@gmail.com>
2026-04-30 19:09:43 +0300

    refactor(organizer-repository): add repository annotation

--- api/src/main/java/school/hei/event_sync/repository/UserRepository.java
+++ api/src/main/java/school/hei/event_sync/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package school.hei.event_sync.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import school.hei.event_sync.model.Organizer;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<Organizer, UUID> {
     Optional<Organizer> findByEmail(String email);
     boolean existsByEmail(String email);
---
+++ api/src/main/java/school/hei/event_sync/service/Authservice.java
@@ -0,0 +1,4 @@
+package school.hei.event_sync.service;
+
+public class Authservice {
+}
