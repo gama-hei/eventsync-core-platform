ac0a4c61b82d9a1ffba06d06d38ca687c137402f
mekill404 <hei.mahery.55@gmail.com>
2026-04-30 20:13:30 +0000

    Add SessionResponse DTO

---
+++ api/src/main/java/school/hei/event_sync/dto/response/SessionResponse.java
@@ -0,0 +1,24 @@
+package school.hei.event_sync.dto.reponse;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SessionResponse {
+    private String id;
+    private String title;
+    private String description;
+    private String startTime;
+    private String endTime;
+    private String roomId;
+    private Integer capacity;
+    private String eventId;
+    private List<SpeakerSummary> speakers;
+    private List<QuestionResponse> questions;
+}
