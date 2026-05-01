78d06bfb24013090314deffda27d83b43463c1e2
mekill404 <hei.mahery.55@gmail.com>
2026-05-01 00:01:15 +0000

    tout est sur render

--- api/src/main/java/school/hei/event_sync/EventSyncApplication.java
+++ api/src/main/java/school/hei/event_sync/EventSyncApplication.java
@@ -2,12 +2,11 @@ package school.hei.event_sync;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.session.jdbc.autoconfigure.JdbcSessionAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = JdbcSessionAutoConfiguration.class)
 public class EventSyncApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EventSyncApplication.class, args);
     }
-
 }
