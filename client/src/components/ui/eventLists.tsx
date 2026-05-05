f9a2a350ac892d292cbd901046c2d0c914ba66cc
Miriasme <johndoe@example.com>
2026-05-05 15:00:24 +0300

    Update : change file name for components eventDetails => eventCard, and eventCard => eventLists

---
+++ client/src/components/ui/eventLists.tsx
@@ -0,0 +1,37 @@
+
+'use client'
+
+import { EventCard} from '@/components/ui/eventCard'
+import { Event } from '@/types/types';
+import { useState } from 'react';
+
+interface Props {
+    events : Event[];
+}
+export default function page({events} : Props) {
+   const [selectedId, setSelectedId] = useState("1");
+
+  const event = events.find((e) => e.id === selectedId);
+  return (
+    <>
+   
+     <div  >
+          <div className="@container/main flex flex-1 flex-col gap-2 pb-20">
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                 {event ? (
+        <EventCard event={event} />
+      ) : (
+        <p>No event found</p>
+      )}
+             
+            </div>
+          </div>
+        </div>
+        
+    </>
+       
+     
+
+  )
+}
+
