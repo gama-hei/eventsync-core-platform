6ee8735a1546b79f011e633c6c94e831228c5f77
Miriasme <johndoe@example.com>
2026-05-19 15:05:26 +0300

    feat(ui): update css for session details in /sessions/page.tsx

--- client/src/app/sessions/[id]/page.tsx
+++ client/src/app/sessions/[id]/page.tsx
@@ -41,9 +41,7 @@ export default function SessionCards() {
   const [error, setError] = useState<string | null>(null);
   const [rooms, setRooms] = useState([]);
   const [roomName, setRoomName] = useState("");
-  const [liked,setLiked] = useState(false)
- 
-    
+  const [liked, setLiked] = useState(false);
 
   useEffect(() => {
     console.log("ID récupéré:", id);
@@ -100,66 +98,75 @@ export default function SessionCards() {
   return (
     <>
       <div className="max-w-6xl mx-auto px-6 py-12">
-        <section className="relative overflow-hidden rounded-3xl bg-[#FBFBFB] border border-gray-100 p-8 md:p-20 mb-20">
+        <section className="relative overflow-hidden rounded-3xl bg-[#FBFBFB] border border-card-last-bg p-8 md:p-20 mb-20">
           <div className="relative z-10 max-w-2xl">
             <Button type="button" className="bg-transparent">
               {live && <Radio className="text-red-500" />}
             </Button>
 
             <div className="inline-flex items-center gap-2 mb-8">
-              <span className="w-8 h-px bg-indigo-600" />
-              <span className="text-xs font-bold uppercase tracking-[0.2em] text-indigo-600">
+              <h1 className="text-sm font-bold uppercase p-2  bg-fill-button lg:text-2xl text-center border-background border-1 rounded-full text-button-blue">
                 Sessions Details
-              </span>
-              <div   className="inline-flex items-center gap-3 text-black px-10 py-4 bg-transparent font-bold text-base  transition-all">
-              
-                  Add to favorite
-                <Heart 
+              </h1>
+              <div className="inline-flex items-center gap-3 text-black px-10 py-4 bg-transparent font-bold text-base  transition-all">
+                Add to favorite
+                <Heart
                   onClick={() => setLiked(!liked)}
-      className={`h-6 w-6 cursor-pointer transition-colors ${
-        liked ? 'fill-red-500 text-red-500' : 'text-gray-400'
-      }`}
+                  className={`h-6 w-6 cursor-pointer transition-colors ${
+                    liked ? "fill-red-500 text-red-500" : "text-gray-400"
+                  }`}
                 />
-                
               </div>
-             
             </div>
             <h2 className="text-2xl md:text-5xl font-serif font-medium text-black mb-8 leading-tight tracking-tight">
               {session.title}
             </h2>
-            <p className="text-xl text-gray-600 font-normal mb-10 leading-relaxed italic">
+          
+              <div className="grid grid-col-3 bg-card-last-bg p-5 gap-5 max-w-64 font-bold rounded-xl text-background mb-12">
+                <h1 className="text-center ">Time & Place</h1>
+                <div className="grid flex-wrap gap-8 mb-12 text-sm font-medium text-gray-500 mx-auto">
+                  <div className="flex items-center gap-2 text-background">
+                    <Clock className="h-4 w-4  group-hover:text-gray-600 transition-colors" />
+                    <span className="font-mono text-sm ">
+                      {formatTime(session.startTime)}
+                    </span>
+                    <span className="text-gray-300">—</span>
+                    <span className="font-mono text-sm ">
+                      {formatTime(session.endTime)}
+                    </span>
+                  </div>
+                  <div className="flex items-center gap-2 text-background">
+                    <Calendar className="h-4 w-4" />{" "}
+                    {formatDate(session.startTime)}{" "}
+                  </div>
+                  <div className="flex items-center gap-2 text-background">
+                    <MapPin className="h-4 w-4 " />
+                    <Link
+                      href={`/rooms/{roomId}/sessions`}
+                      className="underline "
+                    >
+                      {roomName}
+                    </Link>
+                  </div>
+                </div>
+              </div>
+              
+             <div className="grid grid-col-2 gap-3 font-bold">
+              <h1 className="text-xl">SESSION DECRIPTION</h1>
+                <p  className="text-xl text-gray-800 font-normal mb-10 leading-relaxed italic border-2 border-card-last-bg rounded-xl p-3">
               {session.description}
             </p>
-            <div className="flex flex-wrap gap-8 mb-12 text-sm font-medium text-gray-500">
-              <div className="flex items-center gap-2">
-                <Calendar className="h-4 w-4" />{" "}
-                {formatDate(session.startTime)}{" "}
-              </div>
-              <div className="flex items-center gap-2">
-                <Clock className="h-4 w-4 text-gray-400 group-hover:text-gray-600 transition-colors" />
-                <span className="font-mono text-sm text-gray-600">
-                  {formatTime(session.startTime)}
-                </span>
-                <span className="text-gray-300">—</span>
-                <span className="font-mono text-sm text-gray-500">
-                  {formatTime(session.endTime)}
-                </span>
-              </div>
-              <div className="flex items-center gap-2">
-                <MapPin className="h-4 w-4" />
-                <Link href={`/rooms/{roomId}/sessions`} className="underline">
-                  {roomName}
-                </Link>
-              </div>
-            </div>
+              </div>     
+          
+
             <div className="flex flex-wrap gap-2 mb-12 text-sm font-medium text-gray-500">
               <div className="flex items-center gap-2">
-                <Mic2 className="h-6 w-6 text-gray-400 group-hover:text-gray-600 transition-colors" />
+                <Mic2 className="h-6 w-6 text-card-last-bg group-hover:text-gray-600 transition-colors" />
               </div>
-              <div className="flex items-center gap-2">
+              <div className="flex items-center gap-2 font-bold text-xl">
                 Your Speaker :
                 <Link href={`/speakers/${session.speakers[0].id}`}>
-                  <span className="font-mono text-sm text-gray-600 underline">
+                  <span className="font-mono text-m text-gray-600 underline">
                     {session.speakers[0].fullName}
                   </span>
                 </Link>
